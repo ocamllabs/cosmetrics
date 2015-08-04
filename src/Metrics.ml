fd111cee01bf0510c065568e8da6159fdb0e92e4
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-08-05 00:23:30 +0200

    Keep the graph structure of the commits

--- src/Metrics.ml
+++ src/Metrics.ml
@@ -1,15 +1,52 @@
 (** Perform statistics on a set of git repositories. *)
 
 open Lwt
-module T = Irmin.Task
+open CalendarLib
 
-type t = T.t list
+module Commit = struct
+  type t = {
+      date: Calendar.t;
+      author: string;
+      sha1: Irmin.Hash.SHA1.t;
+    }
 
-(** [project] is the directory where the Git repository will be cloned. *)
-let commits ~project remote_uri =
+  let date t = t.date
+  let author t = t.author
+  let sha1 t = t.sha1
+
+  let compare c1 c2 = Irmin.Hash.SHA1.compare c1.sha1 c2.sha1
+  let hash c = Irmin.Hash.SHA1.hash c.sha1
+  let equal c1 c2 = Irmin.Hash.SHA1.equal c1.sha1 c2.sha1
+
+  let of_head t head =
+    Irmin.task_of_head (t "task of head") head >>= fun task ->
+    let t = Int64.to_float(Irmin.Task.date task) in
+    let date = Calendar.from_unixfloat t in
+    let author = Irmin.Task.owner task in
+    return { date; author; sha1 = head }
+end
+
+module History = Graph.Persistent.Digraph.ConcreteBidirectional(Commit)
+
+module M = Map.Make(Irmin.Hash.SHA1)
+
+(* Transform the history from Irmin representation to ours. *)
+let map_history t h0 =
+  (* Collect informations. *)
+  let vertices = Irmin.History.fold_vertex (fun n l -> n :: l) h0 [] in
+  Lwt_list.map_p (Commit.of_head t) vertices >>= fun vertices ->
+  (* Provide an fast access to the vertices. *)
+  let m = List.fold_left (fun m c -> M.add c.Commit.sha1 c m) M.empty vertices in
+  (* Add vertices & edges to the new graph. *)
+  let h = M.fold (fun _ c h -> History.add_vertex h c) m History.empty in
+  let add_edge c1 c2 h = History.add_edge h (M.find c1 m) (M.find c2 m) in
+  let h = Irmin.History.fold_edges add_edge h0 h in
+  return h
+
+let history ?(repo_dir="repo") remote_uri =
   let dir = Filename.basename remote_uri in
   let dir = try Filename.chop_extension dir with _ -> dir in
-  let root = Filename.concat project dir in
+  let root = Filename.concat repo_dir dir in
   let store = Irmin.basic (module Irmin_unix.Irmin_git.FS)
                           (module Irmin.Contents.String) in
   let config = Irmin_unix.Irmin_git.config ~root ~bare:true () in
@@ -20,10 +57,9 @@ let commits ~project remote_uri =
                                remote_uri (Printexc.to_string e))
   >>= fun () ->
   Irmin.history (t "history") >>= fun h ->
-  let c = Irmin.History.fold_vertex (fun v l -> v :: l) h [] in
-  Lwt_list.map_p (fun c -> Irmin.task_of_head (t "task of head") c) c
+  map_history t h
 
-module M = Map.Make(String)
+module MS = Map.Make(String)
 
 type summary = {
     n: int;                     (* total number of commits *)
@@ -32,14 +68,13 @@ type summary = {
 
 (* Similar to "git summary".  To each committer, associate the number
    of commits. *)
-let summary (l: t) =
-  let n = List.length l in
-  let add_commit m c =
-    let author = T.owner c in
-    try M.add author (M.find author m + 1) m
-    with Not_found -> M.add author 1 m in
-  let m = List.fold_left add_commit M.empty l in
-  let authors = M.fold (fun a v l -> (a, v) :: l) m [] in
+let summary h =
+  let n = History.nb_vertex h in
+  let add_commit c m =
+    let a = Commit.author c in
+    try MS.add a (MS.find a m + 1) m with Not_found -> MS.add a 1 m in
+  let m = History.fold_vertex add_commit h MS.empty in
+  let authors = MS.fold (fun a v l -> (a, v) :: l) m [] in
   (* Sort so that more frequent contributors come first. *)
   let authors = List.sort (fun (_,n1) (_,n2) -> compare n2 n1) authors in
   { n; authors }
